8ba588a474e02f4ef23ca3e1788dd2beac14e161
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-01-21 14:55:16 +0100

    feat(auth): add AuthService for user authentication and creation

--- services/AuthService.php
+++ services/AuthService.php
@@ -30,6 +30,8 @@ class AuthService
         $stm = $this->connection->prepare("insert into users (email, password, role) values(?,?,?)");
         $stm->execute([$email,$hash,$role]) ;
     }
+
+    
 }
 
 ?>
