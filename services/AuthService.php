789a5434b4219f7900c4d7e6c4548fa0e2c04b69
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-01-21 14:38:29 +0100

    feat(auth): add AuthService for user authentication and creation

---
+++ services/AuthService.php
@@ -0,0 +1,36 @@
+<?php
+namespace services ;
+use core\Database ;
+use PDO;
+
+class AuthService
+{
+    private $connection ;   
+    public function __construct()
+    {
+        $this->connection =Database::getInstance()->getConnection();
+        }   
+        
+        
+        public function authenticate($email,$password)
+        {
+            $stm = $this->connection->prepare("select *from users where email = ?");
+            $stm->execute([$email]) ;
+            $users = $stm->fetch(PDO::FETCH_ASSOC) ;
+            if($users && password_verify($password  ,$users['password'])) 
+            {
+              return $users ;  
+            }    
+            return false ;
+        }
+
+    public function createUser($email ,$password ,$role)
+    {
+        $hash = password_hash($password,PASSWORD_DEFAULT) ;
+        $stm = $this->connection->prepare("insert into users (email, password, role) values(?,?,?)");
+        $stm->execute([$email,$hash,$role]) ;
+    }
+
+}
+
+?>
